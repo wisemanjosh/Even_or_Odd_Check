edda93fe01c597bbe363aca410c5c20159357f6a
JosueLessie <joslessie@hotmail.com>
2023-10-09 21:36:50 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Even or Odd check
+number = 7
+
+#Calculate the remainder when divided by 2
+remainder = number % 2
+
+# Display whether the number is even or odd
+is_even = remainder == 0
+is_odd = remainder != 0
+
+print("Is even: ", is_even)
+print("Is odd: ", is_odd)
